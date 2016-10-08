ff28af221b3aee9df93e89196d7f3ef3ed49b648
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2016-10-08 21:46:11 +0300

    first commit.

---
+++ JSONandXMLVb/JSONandXMLVb/JSONandXML.asmx.vb
@@ -0,0 +1,27 @@
+﻿Imports System.ComponentModel
+Imports System.Web.Script.Serialization
+Imports System.Web.Script.Services
+Imports System.Web.Services
+
+' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+' <System.Web.Script.Services.ScriptService()> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
+<ToolboxItem(False)> _
+Public Class JSONandXML
+    Inherits System.Web.Services.WebService
+
+    <WebMethod>
+    Public Function GetTeamFromXML() As Member()
+        Dim team As New Team()
+        Return team.GetTeamMembers()
+    End Function
+
+    <WebMethod>
+    <ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+    Public Function GetTeamFromJSON() As String
+        Dim team As New Team()
+        Return New JavaScriptSerializer().Serialize(team.GetTeamMembers())
+    End Function
+
+End Class
---
+++ JSONandXMLVb/JSONandXMLVb/Team.vb
@@ -0,0 +1,10 @@
+﻿Public Class Team
+    Public Function GetTeamMembers() As Member()
+        Dim teamMembers As Member() = New Member() {
+            New Member() With {.Id = 1, .Name = "Samet", .Salary = 10000, .Position = "Software Specialist"},
+            New Member() With {.Id = 2, .Name = "Kubilay", .Salary = 10000, .Position = "Software Specialist"},
+            New Member() With {.Id = 3, .Name = "Erkan", .Salary = 10000, .Position = "Software Specialist"}}
+
+        Return teamMembers
+    End Function
+End Class
